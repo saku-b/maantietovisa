e24441bfbae6dc0000254860730221f588eae5c7
mattinuorela <c3numa02@students.oamk.fi>
2024-12-15 19:04:43 +0200

    muutettu nimet

--- js/koostesivu.js
+++ js/koostesivu.js
@@ -1,11 +1,11 @@
 let tunnista = number(sessionStorage.getItem('tunnista-maa'));
 let kaupungit = number(sessionStorage.getItem('kaupungit'));
 let lippupeli = number(sessionStorage.getItem('lippupeli'));
-let nahtavyydet = number(sessionStorage.getItem('nahtavyydet'));
+let nahtavyydet = number(sessionStorage.getItem('score'));
 
 
 
-let pisteet = tunnista + kaupungit + lippupeli + nahtavyydet;
+let pisteet = tunnista + kaupungit + lippupeli + score;
 
 let pointElem = document.getElementById('points');
 
