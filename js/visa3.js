8944235be733034c0d5ca85515b637dc846e074d
mattinuorela <c3numa02@students.oamk.fi>
2024-12-15 18:06:46 +0200

    pistelasku ja yhtenäistetty eri osiot.

--- js/visa3.js
+++ js/visa3.js
@@ -26,6 +26,14 @@ const landmarks = {
     document.getElementById("option1").textContent = landmarkInfo.options[0];
     document.getElementById("option2").textContent = landmarkInfo.options[1];
     document.getElementById("option3").textContent = landmarkInfo.options[2];
+
+    if (landmark === "Ateena" || landmark === "Machu Picchu" || landmark === "Pyramidit") {
+      const imageElement = document.getElementById("landmark-image");
+      imageElement.src = landmarkInfo.image;
+      imageElement.style.display = "block";
+      imageElement.classList.add("landmark-image");  // Lisää luokka ensimmäisiin kuviin
+    }
+  
   
     document.getElementById("question3-container").dataset.correct = landmarkInfo.correct;
     document.getElementById("question3-container").dataset.image = landmarkInfo.image;
